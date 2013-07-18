0dcd88250e1a9a293d8947b939ba26d9cc1d2ada
Austin Beam <austinbeam@gmail.com>
2013-07-17 23:30:00 -0500

    delay.h: cleaned this up by centralizing defines

--- include/msp/delay.h
+++ include/msp/delay.h
@@ -1,9 +1,11 @@
-// delay.h - delay library providing macros for MSP430 delays. These are for
-// convenience and shouldn't be considered 100% accurate, especially if timing
-// is important. For the most accurate delays, make sure F_CPU matches the
-// operating frequency of your MSP430 device.
+// delay.h - delay library providing MSP430 delay macros and functions. These
+// are for convenience and shouldn't be considered 100% accurate, especially if
+// timing is important. For the most accurate delays, make sure F_CPU matches
+// the operating frequency of your MSP430 device.
 
 #pragma once
+#include "msp/ab2.h"
+#include "../../main.h"
 
 // F_CPU should be defined to the frequency to base delays upon before sourcing
 // the delay library as follows:
@@ -12,20 +14,25 @@
 // Example for 25MHz
 // #define F_CPU 25000000UL
 
-// Delay Functions
+#define F_CPU_NS (F_CPU/1000000000.0)
+#define F_CPU_US (F_CPU/1000000.0)
+#define F_CPU_MS (F_CPU/1000.0)
+#define F_CPU_S  (F_CPU/1.0)
+
+// Delay Macros (more accurate -- static delay required)
 #define _delay_ns(__ns) \
-  if((unsigned long) (F_CPU/1000000000.0 * __ns) != F_CPU/1000000000.0 * __ns)\
-        __delay_cycles((unsigned long) ( F_CPU/1000000000.0 * __ns)+1);\
-  else __delay_cycles((unsigned long) ( F_CPU/1000000000.0 * __ns))
+  if((uint32_t) (F_CPU_NS * __ns) != F_CPU_NS * __ns)\
+        __delay_cycles((uint32_t) ( F_CPU_NS * __ns)+1);\
+  else __delay_cycles((uint32_t) ( F_CPU_NS * __ns))
 #define _delay_us(__us) \
-  if((unsigned long) (F_CPU/1000000.0 * __us) != F_CPU/1000000.0 * __us)\
-        __delay_cycles((unsigned long) ( F_CPU/1000000.0 * __us)+1);\
-  else __delay_cycles((unsigned long) ( F_CPU/1000000.0 * __us))
+  if((uint32_t) (F_CPU_US * __us) != F_CPU_US * __us)\
+        __delay_cycles((uint32_t) ( F_CPU_US * __us)+1);\
+  else __delay_cycles((uint32_t) ( F_CPU_US * __us))
 #define _delay_ms(__ms) \
-  if((unsigned long) (F_CPU/1000.0 * __ms) != F_CPU/1000.0 * __ms)\
-        __delay_cycles((unsigned long) ( F_CPU/1000.0 * __ms)+1);\
-  else __delay_cycles((unsigned long) ( F_CPU/1000.0 * __ms))
+  if((uint32_t) (F_CPU_MS * __ms) != F_CPU_MS * __ms)\
+        __delay_cycles((uint32_t) ( F_CPU_MS * __ms)+1);\
+  else __delay_cycles((uint32_t) ( F_CPU_MS * __ms))
 #define _delay_s(__s) \
-  if((unsigned long) (F_CPU/1.0 * __s) != F_CPU/1.0 * __s)\
-        __delay_cycles((unsigned long) ( F_CPU/1.0 * __s)+1);\
-  else __delay_cycles((unsigned long) ( F_CPU/1.0 * __s))
+  if((uint32_t) (F_CPU_S * __s) != F_CPU_S * __s)\
+        __delay_cycles((uint32_t) ( F_CPU_S * __s)+1);\
+  else __delay_cycles((uint32_t) ( F_CPU_S * __s))
