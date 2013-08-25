8f987ce4e4269ba94bedd51728aeac9866674a8c
Austin Beam <austinbeam@gmail.com>
2013-08-24 20:34:50 -0500

    f5510/rot_enc_test: Modify for int-based enc lib
    Commented out the polling encoder library lines and added the
    interrupt-based equivalents in the rotary encoder test program.

--- src/f5510/rot_enc_test/main.cpp
+++ src/f5510/rot_enc_test/main.cpp
@@ -8,14 +8,18 @@
 // Button callback function declaration
 void doBtn(void);
 // Encoder callback function declaration
-void doEncoder(rot_enc_poll *enc);
+void doEncoder(rot_enc_int *enc);
+//void doEncoder(rot_enc_poll *enc);
 
 // Initialize the clock to F_CPU
 clock clk = clock();
 tlc5925 tlc_red = tlc5925(p4_0);
 tlc5925 tlc_green = tlc5925(p4_2);
 tlc5925 tlc_blue = tlc5925(p4_6);
-rot_enc_poll enc = rot_enc_poll(p1_0, p1_1, &doEncoder);
+
+rot_enc_int enc = rot_enc_int(p1_0, p1_1, &doEncoder);
+//rot_enc_poll enc = rot_enc_poll(p1_0, p1_1, &doEncoder);
+
 int_sw red_sw = int_sw(p1_6, &doBtn);
 
 uint16_t divisor = (ROT_ENC_DIVISIONS / NUM_TLC_LEDs);
@@ -46,7 +50,8 @@ void doBtn(void)
   tlc_green.write(divisor);
 }
 
-void doEncoder(rot_enc_poll *enc)
+void doEncoder(rot_enc_int *enc)
+//void doEncoder(rot_enc_poll *enc)
 {
   tlc_red.write((uint16_t)(1<<((uint16_t)enc->getVal()/divisor)));
 }
--- src/f5510/rot_enc_test/main.h
+++ src/f5510/rot_enc_test/main.h
@@ -6,5 +6,6 @@
 #include "pin_fw.h"
 #include "clock.h"
 #include "msp/tlc5925.h"
+#include "msp/rot_enc_int.h"
 #include "msp/rot_enc_poll.h"
 #include "msp/int_sw.h"
