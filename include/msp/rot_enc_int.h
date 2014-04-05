ee39db8a1f67d6bd9e02c5b07e221c1e6a3065c7
Austin Beam <austinbeam@gmail.com>
2014-04-04 19:18:29 -0500

    msp: rot_enc_int: Remove isr_dispatcher dependency

--- include/msp/rot_enc_int.h
+++ include/msp/rot_enc_int.h
@@ -19,7 +19,6 @@
 #include "ab2.h"
 #include "../clock.h"
 #include "../pin_fw.h"
-#include "../isr_dispatcher.h"
 
 #ifndef ROT_ENC_DIVISIONS
 #define ROT_ENC_DIVISIONS 32
@@ -28,13 +27,9 @@
 class rot_enc_int
 {
 public:
-  // Construct a rotary encoder object with the A and B pins plus a callback to
-  // execute in the ISR.
-  rot_enc_int(msp_pin_t a, msp_pin_t b,
-      void (*callbackFunc)(rot_enc_int *enc),
-      int8_t divs = ROT_ENC_DIVISIONS) :
-      a(a), b(b), callbackFunc(callbackFunc),
-      divs(divs), val(0), prev(0)
+  // Construct a rotary encoder object with the A and B pins
+  rot_enc_int(msp_pin_t a, msp_pin_t b, int8_t divs = ROT_ENC_DIVISIONS) :
+      a(a), b(b), divs(divs), val(0), prev(0)
   {
     // Configure the rotary encoder pin pullups
     pinPullup(a);
@@ -45,27 +40,53 @@ public:
     pinIeEnable(a);
     // Clear the pin A interrupt flag
     pinIfgClear(a);
-
-    // Install the encoder ISR
-    isr_d::install(isr_d::pinVector(a), this, &ISR);
   }
 
-  void   inline clearVal(void)             { val = 0; };
-  int8_t inline getDivs(void)              { return divs;  };
-  int8_t inline getPrev(void)              { return prev;  };
-  int8_t inline getVal(void)               { return val;   };
-  void   inline setDivs(uint16_t new_divs) { divs = new_divs; };
-  void          update(void);
+  void      inline clearVal(void)             { val = 0; };
 
-  // Static wrapper for update to be installed as an ISR
-  static isr_t  ISR(void *encoder);
-
-private:
-  void        (*callbackFunc)(rot_enc_int *enc);
-
+  // We could add getters/setters for each of these, but it doesn't really make
+  // sense in the case where there is no protection needed. Thus, just make
+  // these public to make life easier in the ISR.
   msp_pin_t     a;
   msp_pin_t     b;
   int8_t        divs;
   int8_t        prev;
   int8_t        val;
 };
+
+
+/*
+// Sample initialization and ISR for P1.0
+
+int main(void)
+{
+  // Stuff
+  // Keep in mind 'B' doesn't have to have an interrupt
+  rot_enc_int encoder = rot_enc_int(p1_0, pX_X);
+  // More stuff
+}
+
+// Below is simple for case where only one PORT1 interrupt is enabled. Need to
+// add logic to check the IFG if more than one PORT1 interrupt is in use.
+#pragma vector=PORT1_VECTOR
+void __interrupt intSW_ISR(void)
+{
+  if (!pinRead(encoder.b)) // B is low -> CW
+    encoder.val += 1;
+  else // B is high -> CCW
+    encoder.val -= 1;
+
+  if (encoder.val > (encoder.divs - 1))
+    encoder.val = 0;
+  else if (encoder.val < 0)
+    encoder.val = (encoder.divs - 1);
+
+  if (encoder.val != prev)
+  {
+    // DO STUFF
+  }
+
+  // Save the previous value
+  encoder.prev = encoder.val;
+}
+*/
