6973a5c3948318658ede38e534fccbfec2adaa0d
Alan Bullick <bullick@ab2tech.com>
2014-08-23 00:25:50 -0500

    src: g2553: launchpad: tlc59731: 3rd node breathe

--- src/g2553/launchpad/tlc59731_test/main.cpp
+++ src/g2553/launchpad/tlc59731_test/main.cpp
@@ -4,30 +4,70 @@ int main(void)
 {
   clock(DCO_F_16MHz);
 
-  easyset tlc59731 = easyset(p1_0, 2);
+  easyset tlc59731 = easyset(p1_0, 3);
 
   for (;;) //ever
   {
     for (uint16_t i=0; i<255; i++)
     {
       tlc59731.rgbData(0, i, 0, 0);
-      tlc59731.rgbData(1, 0, i, 0);
+      tlc59731.rgbData(1, i, 0, 0);
+      tlc59731.rgbData(2, i, 0, 0);
       tlc59731.update();
+      clock::delayMS(20);
     }
-    clock::delayS(1);
+    //clock::delayS(1);
     for (uint16_t i=0; i<255; i++)
     {
       tlc59731.rgbData(0, 0, i, 0);
-      tlc59731.rgbData(1, 0, 0, i);
+      tlc59731.rgbData(1, 0, i, 0);
+      tlc59731.rgbData(2, 0, i, 0);
       tlc59731.update();
+      clock::delayMS(20);
     }
-    clock::delayS(1);
+    //clock::delayS(1);
     for (uint16_t i=0; i<255; i++)
     {
       tlc59731.rgbData(0, 0, 0, i);
-      tlc59731.rgbData(1, i, 0, 0);
+      tlc59731.rgbData(1, 0, 0, i);
+      tlc59731.rgbData(2, 0, 0, i);
       tlc59731.update();
+      clock::delayMS(20);
     }
-    clock::delayS(5);
+    for (uint16_t i=0; i<255; i++)
+	{
+	  tlc59731.rgbData(0, i, 0, 0);
+	  tlc59731.rgbData(1, 0, i, 0);
+	  tlc59731.rgbData(2, 0, 0, i);
+	  tlc59731.update();
+	  clock::delayMS(20);
+	}
+	//clock::delayS(1);
+	for (uint16_t i=0; i<255; i++)
+	{
+	  tlc59731.rgbData(0, 0, i, 0);
+	  tlc59731.rgbData(1, 0, 0, i);
+	  tlc59731.rgbData(2, i, 0, 0);
+	  tlc59731.update();
+	  clock::delayMS(20);
+	}
+	//clock::delayS(1);
+	for (uint16_t i=0; i<255; i++)
+	{
+	  tlc59731.rgbData(0, 0, 0, i);
+	  tlc59731.rgbData(1, i, 0, 0);
+	  tlc59731.rgbData(2, 0, i, 0);
+	  tlc59731.update();
+	  clock::delayMS(20);
+	}
+	for (uint16_t i=0; i<255; i++)
+	{
+	  tlc59731.rgbData(0, i, 0, i);
+	  tlc59731.rgbData(1, i, 0, 0);
+	  tlc59731.rgbData(2, 0, i, 0);
+	  tlc59731.update();
+	  clock::delayMS(20);
+	}
+    //clock::delayS(1);
   }
 }
