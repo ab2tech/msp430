6caf67dd9906764a0156e442100ff80888de1e31
Austin Beam <austinbeam@gmail.com>
2014-10-30 21:00:08 -0500

    src: f5510: tlc_test: Example cleanup

--- src/f5510/tlc_test/main.cpp
+++ src/f5510/tlc_test/main.cpp
@@ -3,20 +3,22 @@
 // Initialize the clock
 clock clk = clock();
 
-
-
 int main (void)
 {
+  // Initialize the TLC5925 devices
   tlc5925 tlc_red = tlc5925(p4_0);
   tlc5925 tlc_green = tlc5925(p4_2);
   tlc5925 tlc_blue = tlc5925(p4_6);
+  // Start with CH00
   uint16_t ch = TLC5925_CH00;
   for (;;) //ever
   {
+    // Iterate through the channels by RRC
     ch = rrc(ch);
     tlc_red.write(ch);
     tlc_green.write(ch);
     tlc_blue.write(ch);
+    // 100ms delay between each change for demonstration purposes
     clk.delayMS(100);
   }
 }
