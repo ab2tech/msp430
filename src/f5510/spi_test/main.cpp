030fd20b9635d14db7d406d6776fbdca07a4c854
Austin Beam <austinbeam@gmail.com>
2014-10-30 20:56:56 -0500

    src: f5510: spi_test: Cleanup of example

--- src/f5510/spi_test/main.cpp
+++ src/f5510/spi_test/main.cpp
@@ -2,16 +2,19 @@
 
 // Initialize the clock
 clock clk = clock(F_25MHz);
-spi   test_spi = spi(SPI_B1);
+// Initialize the SPI object
+spi test_spi = spi(SPI_B1);
 
 int main (void)
 {
   uint8_t i = 0;
   for (;;) //ever
   {
+    // Spew 0x0-0xFF for reading on a logic analyzer -- use a delay to make it
+    // easier to look at in the analyzer
     for (i=0; i<0xFF; i++)
     {
-      test_spi.write(i);
+      test_spi.tx(i);
       clk.delayMS(10);
     }
     clk.delayMS(100);
